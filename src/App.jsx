309194d708e585619a86dffadf053fcaaf34fc8c
MuhammedAshiqTKD <akashiq883@gmail.com>
2024-01-27 16:52:41 +0530

    kugu

--- src/App.jsx
+++ src/App.jsx
@@ -265,7 +265,7 @@ function App() {
                     className="icon"
                   />
 
-                  {/* yvuyjhjbjhbjhbhjbuyguyguyg */}
+                  {/* yvuyjhjbjhbjhkjkubhjbuyguyguyg */}
                   <div>
                     <h3>Git</h3>
                     <p>Intermediate</p>
